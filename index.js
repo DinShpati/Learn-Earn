9c110f306bcf43b7230230c485206cf44338ee8f
Din Spataj <proendergamer03@gmail.com>
2020-08-17 21:22:00 +0300

    Full Website

---
+++ index.js
@@ -0,0 +1,17 @@
+window.onscroll = function() {myFunction()};
+
+function myFunction() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    document.getElementById("navbar").classList.add("changeColor");
+  } else {
+    document.getElementById("navbar").classList.remove("changeColor");
+  }
+}
+function toggle() {
+  var x = document.getElementById("myDIV");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
